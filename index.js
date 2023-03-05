726b3da9647ab77aa946b7c749f22f89de74a559
OsamahAlbaghdady <66518676+OsamahAlbaghdady@users.noreply.github.com>
2023-03-06 01:38:06 +0300

    main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-process.env.FFMPEG_PATH = '/var/www/html/newffmpeg/ffmpeg/ffmpeg';
+process.env.FFMPEG_PATH = '/var/www/html/test/ffmpeg-master-latest-linux64-gpl/bin/ffmpeg';
 const express = require('express');
 const app = express();
 const ffmpeg = require('fluent-ffmpeg');
