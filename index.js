18457cc017068af05d3ca997d79ce4c8cc43abe3
osamah <technoabg@gmail.com>
2023-03-05 21:20:32 +0300

    main

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// process.env.FFMPEG_PATH = '/var/www/html/ffmpeg/ffmpeg';
-// const express = require('express');
-// const app = express();
-// const ffmpeg = require('fluent-ffmpeg');
-// const multer = require('multer');
-// const upload = multer({ dest: 'uploads/' }); // Change the upload destination as needed
-
-// // Set the codec to a GPU-accelerated codec
-// const codec = 'h264_nvenc';
-
-// app.post('/upload', upload.single('file'), (req, res) => {
-//     const inputPath = req.file.path;
-//     const outputPath = `./converted/sd${Math.random()}as.mp4`;
-
-//     ffmpeg(inputPath)
-//         .output(outputPath)
-//         .videoBitrate(500)
-//         .withVideoCodec(codec) // Use GPU-accelerated codec
-//         .on('end', () => {
-//             console.log('Video converted and saved successfully');
-//             res.send(outputPath);
-//         })
-//         .on('error', (error) => {
-//             console.error(`Error converting video: ${error.message}`);
-//             res.status(500).send('Error converting video');
-//         })
-//         .run();
-// });
-
-// app.listen(3000, () => {
-//     console.log('mdsjifhb');
-// })
-
-
-
 process.env.FFMPEG_PATH = '/var/www/html/ffmpeg/ffmpeg';
 const express = require('express');
 const app = express();
@@ -40,38 +5,27 @@ const ffmpeg = require('fluent-ffmpeg');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Change the upload destination as needed
 
-//ffmpeg.setFfmpegPath('/var/www/html/ffmpeg/ffmpeg');
 app.post('/upload', upload.single('file'), (req, res) => {
     const inputPath = req.file.path;
     const outputPath = `./converted/sd${Math.random()}as.mp4`;
 
-    const command = ffmpeg(inputPath)
+    ffmpeg(inputPath)
         .output(outputPath)
         .videoBitrate(500)
-        .outputOptions('-f', 'mp4')
-        .outputOptions('-c:v', 'h264')
-        .outputOptions('-profile:v', 'baseline')
-        .outputOptions('-level', '3.0')
-        .outputOptions('-preset', 'superfast')
-        .outputOptions('-crf', '22')
-        .outputOptions('-strict', 'experimental')
-        .outputOptions('-c:a', 'aac')
-        .outputOptions('-b:a', '128k')
-        .outputOptions('-f', 'mp4')
-        .output('pipe:1')
+        .outputOptions([
+            '-c:v', 'h264_cuvid',
+            '-resize', '1280x720',
+            '-c:a', 'copy'
+          ])
         .on('end', () => {
             console.log('Video converted and saved successfully');
             res.send(outputPath);
         })
         .on('error', (error) => {
-            console.log(error);
             console.error(`Error converting video: ${error.message}`);
             res.status(500).send('Error converting video');
-        });
-
-    console.log('FFmpeg command:' );
-
-    command.run();
+        })
+        .run();
 });
 
 app.listen(3000, () => {
