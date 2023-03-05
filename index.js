563c9415d2f32e5d0662b8b005a4951d6a831587
osamah <technoabg@gmail.com>
2023-03-05 21:52:15 +0300

    main

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 process.env.FFMPEG_PATH = '/var/www/html/ffmpeg/ffmpeg';
 const express = require('express');
 const app = express();
+const exec = require('exec')
 const ffmpeg = require('fluent-ffmpeg');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Change the upload destination as needed
+
+
 
 app.post('/upload', upload.single('file'), (req, res) => {
     const inputPath = req.file.path;
     const outputPath = `./converted/sd${Math.random()}as.mp4`;
 
+
     ffmpeg(inputPath)
         .output(outputPath)
+        .addOptions([
+            '-hwaccel_device', '0',
+            '-hwaccel', 'cuvid',
+            '-c:v', 'h264_cuvid',
+        ])
         .videoBitrate(500)
-        .outputOptions([
-            '-c:v', 'h264_nvenc',
-          //  '-resize', '1280x720',
-            '-c:a', 'copy'
-          ])
+        .videoCodec('h264_nvenc')
         .on('end', () => {
             console.log('Video converted and saved successfully');
             res.send(outputPath);
