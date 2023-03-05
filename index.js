b28253c3a2842d44c8befab13d980f6c70f679ce
osamah <technoabg@gmail.com>
2023-03-05 21:04:20 +0300

    main

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// process.env.FFMPEG_PATH = '/var/www/html/ffmpeg/ffmpeg';
+// const express = require('express');
+// const app = express();
+// const ffmpeg = require('fluent-ffmpeg');
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' }); // Change the upload destination as needed
+
+// // Set the codec to a GPU-accelerated codec
+// const codec = 'h264_nvenc';
+
+// app.post('/upload', upload.single('file'), (req, res) => {
+//     const inputPath = req.file.path;
+//     const outputPath = `./converted/sd${Math.random()}as.mp4`;
+
+//     ffmpeg(inputPath)
+//         .output(outputPath)
+//         .videoBitrate(500)
+//         .withVideoCodec(codec) // Use GPU-accelerated codec
+//         .on('end', () => {
+//             console.log('Video converted and saved successfully');
+//             res.send(outputPath);
+//         })
+//         .on('error', (error) => {
+//             console.error(`Error converting video: ${error.message}`);
+//             res.status(500).send('Error converting video');
+//         })
+//         .run();
+// });
+
+// app.listen(3000, () => {
+//     console.log('mdsjifhb');
+// })
+
+
+
 process.env.FFMPEG_PATH = '/var/www/html/ffmpeg/ffmpeg';
 const express = require('express');
 const app = express();
@@ -5,17 +40,25 @@ const ffmpeg = require('fluent-ffmpeg');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // Change the upload destination as needed
 
-// Set the codec to a GPU-accelerated codec
-const codec = 'h264_nvenc';
-
+//ffmpeg.setFfmpegPath('/var/www/html/ffmpeg/ffmpeg');
 app.post('/upload', upload.single('file'), (req, res) => {
     const inputPath = req.file.path;
     const outputPath = `./converted/sd${Math.random()}as.mp4`;
 
-    ffmpeg(inputPath)
+    const command = ffmpeg(inputPath)
         .output(outputPath)
         .videoBitrate(500)
-        .withVideoCodec(codec) // Use GPU-accelerated codec
+        .outputOptions('-f', 'mp4')
+        .outputOptions('-c:v', 'h264')
+        .outputOptions('-profile:v', 'baseline')
+        .outputOptions('-level', '3.0')
+        .outputOptions('-preset', 'superfast')
+        .outputOptions('-crf', '22')
+        .outputOptions('-strict', 'experimental')
+        .outputOptions('-c:a', 'aac')
+        .outputOptions('-b:a', '128k')
+        .outputOptions('-f', 'mp4')
+        .output('pipe:1')
         .on('end', () => {
             console.log('Video converted and saved successfully');
             res.send(outputPath);
@@ -24,8 +67,11 @@ app.post('/upload', upload.single('file'), (req, res) => {
             console.log(error);
             console.error(`Error converting video: ${error.message}`);
             res.status(500).send('Error converting video');
-        })
-        .run();
+        });
+
+    console.log('FFmpeg command:', command.spawnArgs().join(' '));
+
+    command.run();
 });
 
 app.listen(3000, () => {
